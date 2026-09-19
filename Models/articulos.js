c4b904dc7d63a5b4af69ac5047763877606e01a1
ikay69 <ikaydev59@gmail.com>
2026-09-19 17:35:48 -0500

    mejora salida articulos activos

--- Models/articulos.js
+++ Models/articulos.js
@@ -167,6 +167,7 @@ const Articulos = {
                 a.Estado            AS artEstado,
                 c.Nombre            AS artCategoria,
                 tp.Nombre           AS artTipoProducto,
+                u.Nombre            AS artUnidadMedida,
                 IFNULL(
                     GROUP_CONCAT(CONCAT(pr.Nombre, ':', ap.Valor) SEPARATOR ','),
                     ''
@@ -175,11 +176,12 @@ const Articulos = {
                 LEFT JOIN Productos p ON p.Id = a.ProductoId
                 LEFT JOIN TiposProductos tp ON tp.Id = p.TipoProductoId
                 LEFT JOIN Categorias c ON c.Id = p.CategoriaId
+                LEFT JOIN UnidadesMedidas u ON u.Id = p.UnidadMedidaId
                 LEFT JOIN ArticuloPropiedades ap ON ap.ArticuloId = a.Id
                 LEFT JOIN Propiedades pr ON pr.Id = ap.PropiedadId
             WHERE a.EmpresaId = ? AND a.Estado = true AND a.Vender = true
                 AND a.${pCampoOrden} LIKE ?
-            GROUP BY  a.Id, c.Nombre, tp.Nombre
+            GROUP BY  a.Id, c.Nombre, tp.Nombre, u.Nombre
             ORDER BY a.${pCampoOrden} ${pOrden}
             LIMIT 50 OFFSET ?;`,
             [pEmpId,pTexto,pOffset]
